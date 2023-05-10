360b8713bdda5949aa06152473cd2e785cfab4c1
Athira K S <athiraks@Athiras-MacBook-Air.local>
2023-05-10 16:25:42 +0530

    Mobile view

--- status-card/src/app/status-card/status-card.component.ts
+++ status-card/src/app/status-card/status-card.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-status-card',
   templateUrl: './status-card.component.html',
-  styleUrls: ['./status-card.component.scss']
+  styleUrls: ['./status-card.component.scss'],
 })
 export class StatusCardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
